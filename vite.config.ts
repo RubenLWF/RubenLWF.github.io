e8eb80713ecd243a5db658cf5368eb143d40218f
Ruben Lagerwerf <ruben.lagerwerf@gmail.com>
2025-08-02 17:08:27 +0200

    feat: move api proxy over to vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,34 +6,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/',
-  server: {
-    proxy: {
-      '/api/spotify/token': {
-        target: 'https://accounts.spotify.com/api/token',
-        changeOrigin: true,
-        secure: true,
-        cookieDomainRewrite: false,
-        rewrite: (path) => path.replace(/^\/api\/spotify\/token/, ''),
-        configure: (proxy, _options) => {
-          proxy.on('proxyRes', (proxyRes) => {
-            // Remove problematic cookies
-            delete proxyRes.headers['set-cookie'];
-          });
-        }
-      },
-      '/api/spotify/v1': {
-        target: 'https://api.spotify.com/v1',
-        changeOrigin: true,
-        secure: true,
-        cookieDomainRewrite: false,
-        rewrite: (path) => path.replace(/^\/api\/spotify\/v1/, ''),
-        configure: (proxy, _options) => {
-          proxy.on('proxyRes', (proxyRes) => {
-            // Remove problematic cookies
-            delete proxyRes.headers['set-cookie'];
-          });
-        }
-      }
-    }
-  }
 })
